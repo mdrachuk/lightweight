13f2b85facfa5bfeb1dafbfc2c5b5b561d3afef1
mdrachuk <misha@drach.uk>
2019-08-22 18:04:05 +0300

    Test rewrite

---
+++ tests/test_other.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from lightweight import Site
+
+
+def test_rewrite_out(tmp_path: Path):
+    assert_site_render('resources/test1/test2/test3/test.html', 'resources/test1', tmp_path)
+    assert_site_render('resources/test.html', 'resources/test.html', tmp_path)
+
+
+def assert_site_render(src_location, include, tmp_path):
+    with Path(src_location).open() as f:
+        src_content = f.read()
+    out_path = tmp_path / 'out'
+    site = Site(out_path)
+    site.include(include)
+    site.render()
+    assert (out_path / src_location).exists()
+    assert (out_path / src_location).read_text() == src_content
