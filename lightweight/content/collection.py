007f80c60e505081c2a6e7b743fbaeb82b5b8b97
mdrachuk <misha@drach.uk>
2019-09-17 19:54:40 +0300

    Document content collections

--- lightweight/content/collection.py
+++ lightweight/content/collection.py
@@ -4,7 +4,7 @@ from abc import ABC
 from datetime import datetime
 from pathlib import Path
 from typing import Mapping, Dict, TYPE_CHECKING, Collection, Iterator, Any, Tuple
-from urllib.parse import urlparse, urlunparse, urljoin
+from urllib.parse import urljoin
 
 if TYPE_CHECKING:
     from lightweight import Content
@@ -16,24 +16,50 @@ Email = str
 
 
 class ContentCollection:
+    """A collection of Content that can be queried and iterated."""
+
     def __init__(self, content: Mapping[Path, Content]):
         self.content = dict(content)
 
     def __getitem__(self, path_part: str) -> ContentAtPath:
-        content = self.content_at_path(path_part)
+        """Get a collection of content included at provided path.
+
+            :Example:
+            >>> site = Site(url='http://example.org', ...) # site is a collection of content
+
+            >>> site.include('index.html', <index>)
+            >>> site.include('posts/1', <post-1>)
+            >>> site.include('posts/2', <post-2>)
+            >>> site.include('posts/3', <post-3>)
+            >>> site.include('static', <static>)
+
+            >>> posts = site['posts']
+            >>> assert posts.url is 'http://example.org/posts'
+            >>> assert <post-1> in posts
+            >>> assert <post-2> in posts
+            >>> assert <post-3> in posts
+            >>> assert <static> not in posts
+        """
+
+        content = self.content_at_path(Path(path_part))
         if not content:
             raise KeyError(f'There is no content at path "{path_part}"')
         return ContentAtPath(self, Path(path_part), content)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Content]:
+        """Iterate `Content` objects in this collection."""
         return iter(self.content.values())
 
-    def content_at_path(self, path_part):
+    def content_at_path(self, target: Path):
+        index = len(target.parts)
         return {
             path: content
             for path, content in self.content.items()
-            if path.parts[0] == path_part
+            if len(path.parts) > index and all(actual == expected for actual, expected in zip(path.parts, target.parts))
         }
+
+    def __contains__(self, content: Content) -> bool:
+        return content in self.content.values()
 
 
 class EntriesCollection(ABC):
@@ -69,21 +95,35 @@ class EntriesCollection(ABC):
 
 
 class ContentAtPath(EntriesCollection, ContentCollection):
+    """Content Collection retrieved by accessing other content collection by path:
 
-    def __init__(self, root: ContentCollection, path: Path, content: Dict[Path, Content]):
+            :Example:
+            >>> site = Site(url='http://example.org', ...) # site is a collection of content
+
+            >>> site.include('index.html', <index>)
+            >>> site.include('posts/dev/1', <post-1>)
+            >>> site.include('posts/dev/2', <post-2>)
+            >>> site.include('posts/other/3', <post-3>)
+            >>> site.include('static', <static>)
+
+            >>> posts = site['posts']
+            >>> dev_posts = posts['dev']
+            >>> assert dev_posts.url is 'http://example.org/posts/dev'
+            >>> assert <post-1> in posts
+            >>> assert <post-2> in posts
+            >>> assert <post-3> not in posts
+            >>> assert <static> not in posts
+    """
+
+    def __init__(self, source: ContentCollection, path: Path, content: Dict[Path, Content]):
         super().__init__(content)
-        self.root = root
         self.relative_path = path
-        self.base_size = len(self.relative_path.parts)
 
-        self.take_after(root)
-        root_url = getattr(root, 'url', '')
+        self.take_after(source)
+        root_url = getattr(source, 'url', '')
         self.url = urljoin(root_url, str(path))
         self.description = f'{self.title} | {self.relative_path}'
 
     def __getitem__(self, path_part: str):
-        return ContentAtPath(self.root, self.relative_path / Path(path_part), {
-            path: content
-            for path, content in self.content.items()
-            if len(path.parts) > self.base_size and path.parts[self.base_size] == path_part
-        })
+        path = self.relative_path / path_part
+        return ContentAtPath(self, path, self.content_at_path(path))
