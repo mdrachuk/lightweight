f55af602ffcc7f45a77eeefd0de5b2a011682f5c
mdrachuk <misha@drach.uk>
2019-09-26 00:47:48 +0300

    Add url filter

--- lightweight/template.py
+++ lightweight/template.py
@@ -1,14 +1,23 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, Any
 
-from jinja2 import Environment, FileSystemLoader, Template
+from jinja2 import Environment, FileSystemLoader, Template, contextfilter
 
-jinja_templates = Environment(loader=FileSystemLoader('./.', followlinks=True))
+jinja = Environment(loader=FileSystemLoader('./.', followlinks=True))
+
+
+@contextfilter
+def url(ctx: Any, path: Path) -> str:
+    location = str(path.with_suffix('') if path.suffix == '.html' else path)
+    return f'{ctx["site"].url}/{location}'
+
+
+jinja.filters['url'] = url
 
 
 def template(name: Union[str, Path], base_dir: Union[str, Path] = 'templates') -> Template:
     """A shorthand for loading a Jinja2 template from `templates` directory."""
-    return jinja_templates.get_template(str(_concat(base_dir, name)))
+    return jinja.get_template(str(_concat(base_dir, name)))
 
 
 def _concat(base: Union[str, Path], path: Union[str, Path]):
