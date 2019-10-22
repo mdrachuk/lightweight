c0f5af9fb1d465cb7a8a900103356fc56f608db7
mdrachuk <misha@drach.uk>
2019-10-22 20:20:34 +0300

    Only relative templates

--- lightweight/template.py
+++ lightweight/template.py
@@ -8,13 +8,5 @@ jinja = Environment(loader=FileSystemLoader('./.', followlinks=True))
 
 def template(name: Union[str, Path], base_dir: Union[str, Path] = 'templates') -> Template:
     """A shorthand for loading a Jinja2 template from `templates` directory."""
-    return jinja.get_template(str(_concat(base_dir, name)))
-
-
-def _concat(base: Union[str, Path], path: Union[str, Path]):
-    base = Path(base)
-    path = Path(path)
-    if path.is_absolute():
-        return path
-    else:
-        return base / path
+    location = str(Path(base_dir) / Path(name))
+    return jinja.get_template(location)
