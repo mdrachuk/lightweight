6d3a39d9efe2d0cee9a22f26e36a9e365d6721d1
mdrachuk <misha@drach.uk>
2019-11-12 16:13:17 +0200

    Extract lightweight example to separate project

--- examples/lightweight-home/locate_lightweight_for_example.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-
-def update_path() -> None:
-    _fp = Path(os.path.realpath(__file__))
-    os.chdir(_fp.parent)
-    sys.path.append(str(_fp.parent.parent.parent))
