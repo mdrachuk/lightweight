65d2b69792109036a55d070c747e93667a28c938
mdrachuk <misha@drach.uk>
2019-12-11 16:04:13 +0200

    Rename test param

--- tests/test_other.py
+++ tests/test_other.py
@@ -8,12 +8,12 @@ def test_rewrite_out(tmp_path: Path):
     assert_site_render('resources/test.html', 'resources/test.html', tmp_path)
 
 
-def assert_site_render(src_location, include, tmp_path):
+def assert_site_render(src_location, content, tmp_path):
     with Path(src_location).open() as f:
         src_content = f.read()
     test_out = tmp_path / 'out'
     site = Site(url='https://example.com')
-    site.include(include)
+    site.include(content)
     site.render(test_out)
     assert (test_out / src_location).exists()
     assert (test_out / src_location).read_text() == src_content
