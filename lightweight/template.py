a283d23aa12056f1521f2ae248f35288759b87a0
mdrachuk <misha@drach.uk>
2019-12-24 19:48:25 +0200

    Cast DynamicCwd to str

--- lightweight/template.py
+++ lightweight/template.py
@@ -1,16 +1,17 @@
 from os import PathLike, getcwd
 from pathlib import Path
-from typing import Union
+from typing import Union, cast
 
 from jinja2 import Environment, FileSystemLoader, Template
 
 
 class DynamicCwd(PathLike):
+    # TODO:mdrachuk:24.12.2019: implement str interface (mostly, hash should probably raise an error, etc)
     def __fspath__(self):
         return getcwd()
 
 
-cwd_loader = FileSystemLoader([DynamicCwd()], followlinks=True)
+cwd_loader = FileSystemLoader([cast(str, DynamicCwd())], followlinks=True)
 jinja = Environment(loader=cwd_loader, cache_size=0)
 
 
