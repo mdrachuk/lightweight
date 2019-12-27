f225415a1b7463b57fb592c454f6b68517ab261b
mdrachuk <misha@drach.uk>
2019-12-27 21:06:19 +0200

    Test include copy under different path

--- tests/test_include_copy.py
+++ tests/test_include_copy.py
@@ -54,3 +54,42 @@ def test_include_glob(tmp_path: Path):
     assert (result / 'a.html').exists()
     assert (result / 'b.html').exists()
     assert (result / 'dir' / '1.html').exists()
+
+
+def test_include_file_under_different_name(tmp_path: Path):
+    src_location = 'resources/test.html'
+    src_path = Path(src_location)
+    with src_path.open() as f:
+        src_content = f.read()
+
+    test_out = tmp_path / 'out'
+    site = Site(url='https://example.com')
+
+    site.include('a.html', src_location)
+    site.render(test_out)
+
+    assert (test_out / 'a.html').exists()
+    assert (test_out / 'a.html').read_text() == src_content
+
+
+def test_include_dir_under_different_name(tmp_path: Path):
+    src_location = 'resources/test_nested/test2/test3/test.html'
+    src_path = Path(src_location)
+    with src_path.open() as f:
+        src_content = f.read()
+
+    test_out = tmp_path / 'out'
+    site = Site(url='https://example.com')
+
+    site.include('successful_test', 'resources/test_nested')
+    site.render(test_out)
+
+    assert (test_out / 'successful_test/test2/test3/test.html').exists()
+    assert (test_out / 'successful_test/test2/test3/test.html').read_text() == src_content
+
+
+def test_include_not_found_under_different_name(tmp_path: Path):
+    site = Site(url='https://example.com')
+
+    with pytest.raises(FileNotFoundError):
+        site.include('t.html', str(uuid4()))
