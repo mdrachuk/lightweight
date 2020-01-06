e40a5ed39ba60f7fbc0501d9879bd7bcf404f337
mdrachuk <misha@drach.uk>
2020-01-06 17:15:24 +0200

    Remove unused code

--- lightweight/content/content.py
+++ lightweight/content/content.py
@@ -1,32 +1,15 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from lightweight import GenPath, GenContext
 
-# Type aliases for clear type definitions
-Url = str
-Email = str
-
 
 class Content(ABC):
+    """An abstract content that can be included by a Site."""
 
     @abstractmethod
     def write(self, path: GenPath, ctx: GenContext):
-        """Write...
-        """
-
-
-class Entry(ABC):
-    """A definition of an Entry interface. Properties defined by Entry can be used by other modules, e.g. feed."""
-
-    url: Url
-    title: str
-    content: str
-    author_name: str
-    author_email: Email
-    created: datetime
-    updated: datetime
+        """Write the content to the file at path."""
