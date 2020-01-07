b6a28b813f3b72c28c6e3d1bc468d3fb639ad4af
mdrachuk <misha@drach.uk>
2020-01-07 14:04:49 +0200

    Test GenPath.url

--- tests/test_other.py
+++ tests/test_other.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from lightweight import Site, Author
+from lightweight import Site, Author, GenPath
 from lightweight.errors import AbsolutePathIncluded
 
 
@@ -70,3 +70,17 @@ def test_site_authors_combination():
                             Author('a', 'a@example.org'),
                             Author('b', 'b@example.org'),
                             Author('c', 'c@example.org')}
+
+
+def test_gen_path_url(tmp_path: Path):
+    site = Site('https://example.org/')
+    url_factory = lambda location: site / location
+    out = tmp_path
+
+    page = GenPath(Path('page.html'), out, url_factory)
+    index = GenPath(Path('index.html'), out, url_factory)
+    style = GenPath(Path('style.css'), out, url_factory)
+
+    assert style.url == 'https://example.org/style.css'
+    assert page.url == 'https://example.org/page'
+    assert index.url == 'https://example.org/'
