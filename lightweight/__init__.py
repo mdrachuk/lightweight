c09f4a1ced69421a5703e413393bcf668724f2af
mdrachuk <misha@drach.uk>
2020-01-08 16:13:55 +0200

    Document template

--- lightweight/__init__.py
+++ lightweight/__init__.py
@@ -1,7 +1,44 @@
-from .site import Site, Author
+"""Lightweight is a "Code over configuration" static site generator.
+
+@example
+```python
+from lightweight import Site, markdown, paths, jinja, template, rss, atom, sass
+
+
+def blog_posts(source):
+    post_template = template('posts/_template.html')
+    # Use globs to select files. # source = 'posts/**.md'
+    return (markdown(path, post_template) for path in paths(source))
+
+
+site = Site(url='https://example.org/')
+
+# Render an index page from Jinja2 template.
+site.include('index.html', jinja('pages/index.html'))
+
+# Render markdown blog posts.
+[site.include(f'posts/{post.source_path.stem}.html', post) for post in blog_posts('posts/**.md')]
+site.include('posts.html', jinja('pages/posts.html'))
+
+# Syndicate RSS and Atom feeds.
+site.include('posts.atom.xml', atom(site['posts']))
+site.include('posts.rss.xml', rss(site['posts']))
+
+# Render SASS to CSS.
+site.include('css/style.css', sass('styles/style.scss'))
+
+# Include a copy of a directory.
+site.include('img')
+site.include('js')
+
+# Execute all included content.
+site.generate()
+```
+"""
 from .content import Content, feeds, atom, rss, markdown, jinja, from_ctx, sass
 from .files import paths, directory
 from .generation import GenPath, GenContext
-from .template import template, jinja_env
+from .site import Site, Author
+from .template import template, lw_jinja
 
 __version__ = '1.0.0.dev34'
--- lightweight/template.py
+++ lightweight/template.py
@@ -1,23 +1,27 @@
 from os import PathLike, getcwd
 from pathlib import Path
-from typing import Union, cast, TYPE_CHECKING
+from typing import Union, cast
 
 from jinja2 import Environment, FileSystemLoader, Template
 
-if TYPE_CHECKING:
-    pass
-
 
 class DynamicCwd(PathLike):
+    """This is a path, that upon use always points to the current directory."""
+
     # TODO:mdrachuk:24.12.2019: implement str interface (mostly, hash should probably raise an error, etc)
     def __fspath__(self):
         return getcwd()
 
 
+#
+# This bit may look a bit dodgy.
+# The thing is, for an ability to have isolated subsites, but still use the simple approach with a global Jinja
+# environment the environment is set to load templates relative to current working directory.
+#
 cwd_loader = FileSystemLoader([cast(str, DynamicCwd())], followlinks=True)
-jinja_env = Environment(loader=cwd_loader, cache_size=0)
+lw_jinja = Environment(loader=cwd_loader, cache_size=0)
 
 
 def template(location: Union[str, Path]) -> Template:
-    """A shorthand for loading a Jinja2 template from `templates` directory."""
-    return jinja_env.get_template(str(location))
+    """A shorthand for loading a Jinja2 template from the current working directory."""
+    return lw_jinja.get_template(str(location))
