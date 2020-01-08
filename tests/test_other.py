022397220c6b9960c54beedb42097e8ae0bf9a81
mdrachuk <misha@drach.uk>
2020-01-08 15:11:07 +0200

    Test GenPath.url and .location

--- tests/test_other.py
+++ tests/test_other.py
@@ -84,3 +84,17 @@ def test_gen_path_url(tmp_path: Path):
     assert style.url == 'https://example.org/style.css'
     assert page.url == 'https://example.org/page'
     assert index.url == 'https://example.org/'
+
+
+def test_gen_path_location(tmp_path: Path):
+    site = Site('https://example.org/')
+    url_factory = lambda location: site / location
+    out = tmp_path
+
+    page = GenPath(Path('page.html'), out, url_factory)
+    index = GenPath(Path('index.html'), out, url_factory)
+    style = GenPath(Path('style.css'), out, url_factory)
+
+    assert style.location == 'style.css'
+    assert page.location == 'page'
+    assert index.location == ''
