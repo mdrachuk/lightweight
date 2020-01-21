fd7e0bca5bfb3ef7aee63aeeb681355a3db3df95
mdrachuk <misha@drach.uk>
2020-01-21 23:55:30 +0200

    Execute tasks by cwd especially for site

--- tests/test_site_nesting.py
+++ tests/test_site_nesting.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from lightweight import Site
+from lightweight import Site, directory
 from lightweight.content.copies import FileCopy
 
 
@@ -20,3 +20,13 @@ def test_include_site(tmp_path: Path):
     root.generate(tmp_path)
     assert (test_out / 'child/test.html').exists()
     assert (test_out / 'child/test.html').read_text() == src_content
+
+
+def test_subsite_cwd_change(tmp_path: Path):
+    site = Site('https://example.org/')
+    site.include('index.html', 'site/index.html')
+    with directory('site'):
+        subsite = Site('https://example.org/')
+        subsite.include('file')
+    site.include('subsite', subsite)
+    site.generate(out=tmp_path)
