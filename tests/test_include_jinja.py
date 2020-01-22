5d14f4f57279a580a3933366190b74481c073e9a
mdrachuk <misha@drach.uk>
2020-01-22 09:29:29 +0200

    Move jinja test to other jinja tests

--- tests/test_include_jinja.py
+++ tests/test_include_jinja.py
@@ -1,10 +1,11 @@
+import asyncio
 from os import chdir, getcwd
 from pathlib import Path
 
 import pytest
-from jinja2 import TemplateNotFound
+from jinja2 import TemplateNotFound, UndefinedError
 
-from lightweight import Site, jinja, Content, directory, GenContext, GenPath, from_ctx
+from lightweight import Site, jinja, Content, directory, GenContext, GenPath, from_ctx, jinja_env
 
 
 def test_render_jinja(tmp_path: Path):
@@ -85,3 +86,10 @@ def test_lazy_params(tmp_path: Path):
     assert (test_out / out_location).exists()
     with open('expected/jinja/lazy.html') as expected:
         assert (test_out / out_location).read_text() == expected.read()
+
+
+def test_jinja_env_does_not_allow_undefined():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    with pytest.raises(UndefinedError):
+        jinja_env.from_string('{{something}}').render()
