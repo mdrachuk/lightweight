362626354e305563d946aeff3dace318065f4c09
mdrachuk <misha@drach.uk>
2020-01-27 16:15:56 +0200

    Shutdown the server during tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import gather, Task
 from pathlib import Path
 from urllib.parse import urlsplit
 
@@ -7,7 +8,19 @@ import pytest
 from lightweight.server import LIVE_RELOAD_JS, DevServer, LiveReloadServer
 
 
-class TestServer:
+class TestTheServer:
+
+    @pytest.fixture()
+    def event_loop(self):
+        self.server = None
+        loop = asyncio.new_event_loop()
+        yield loop
+        if self.server:
+            self.server.shutdown()
+        pending = asyncio.all_tasks(loop=loop)
+        loop.run_until_complete(gather(*pending, loop=loop))
+        loop.close()
+
     @pytest.fixture(autouse=True)
     def _create_tmp_site(self, tmp_path: Path):
         self.dir_path = tmp_path
@@ -19,26 +32,27 @@ class TestServer:
 
     @pytest.mark.asyncio
     async def test_serves_live_reload_js(self, event_loop, unused_tcp_port):
-        server = LiveReloadServer(
+        loop = event_loop
+        self.server = LiveReloadServer(
             self.directory,
             watch=self.directory,
             regenerate=lambda: None,
             ignored=[]
         )
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=loop)
 
         assert 'liveReload.start();' in await get(f'http://127.0.0.1:{port}')
         assert LIVE_RELOAD_JS in await get(f'http://127.0.0.1:{port}')
         assert LIVE_RELOAD_JS in await get(f'http://127.0.0.1:{port}/index')
         assert LIVE_RELOAD_JS in await get(f'http://127.0.0.1:{port}/index.html')
-        identifier = server.live_reload_id
+        identifier = self.server.live_reload_id
         assert identifier in await get(f'http://127.0.0.1:{port}/__live_reload_id__')
 
         with (self.dir_path / 'new-file').open('w') as f:
             f.write('Test file changes')
         await asyncio.sleep(0.5)  # wait for file change to get picked up
-        new_identifier = server.live_reload_id
+        new_identifier = self.server.live_reload_id
         assert new_identifier in await get(f'http://127.0.0.1:{port}/__live_reload_id__')
         assert identifier != new_identifier
 
@@ -53,9 +67,9 @@ class TestServer:
                 self.called = True
 
         regenerate = MockRegenerate()
-        server = LiveReloadServer(self.directory, watch=self.directory, regenerate=regenerate, ignored=[])
+        self.server = LiveReloadServer(self.directory, watch=self.directory, regenerate=regenerate, ignored=[])
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=event_loop)
 
         assert await get(f'http://127.0.0.1:{port}/__live_reload_id__')
         with (self.dir_path / 'new-file').open('w') as f:
@@ -67,27 +81,27 @@ class TestServer:
     async def test_live_reload_ignore(self, event_loop, unused_tcp_port):
         ignored_path = self.dir_path / 'ignore'
         ignored_path.mkdir(parents=True, exist_ok=True)
-        server = LiveReloadServer(
+        self.server = LiveReloadServer(
             self.directory,
             watch=self.directory,
             regenerate=lambda: None,
             ignored=[str(ignored_path)]
         )
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=event_loop)
 
-        identifier = server.live_reload_id
+        identifier = self.server.live_reload_id
         assert identifier in await get(f'http://127.0.0.1:{port}/__live_reload_id__')
         with (ignored_path / 'ignored.txt').open('w') as f:
             f.write('Ignored file changes')
         await asyncio.sleep(0.5)  # wait for file change to get picked up
-        assert identifier == server.live_reload_id  # does not change
+        assert identifier == self.server.live_reload_id  # does not change
 
     @pytest.mark.asyncio
     async def test_serves_no_live_reload_js(self, event_loop, unused_tcp_port):
-        server = DevServer(self.directory)
+        self.server = DevServer(self.directory)
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=event_loop)
 
         assert LIVE_RELOAD_JS not in await get(f'http://127.0.0.1:{port}/')
         assert LIVE_RELOAD_JS not in await get(f'http://127.0.0.1:{port}/index.')
@@ -98,21 +112,30 @@ class TestServer:
 
     @pytest.mark.asyncio
     async def test_403(self, event_loop, unused_tcp_port):
-        server = DevServer(self.directory)
+        self.server = DevServer(self.directory)
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=event_loop)
 
         assert '403' in await get(f'http://127.0.0.1:{port}/../..')
 
     @pytest.mark.asyncio
-    async def test_500(self, event_loop, unused_tcp_port):
+    async def test_500(self, event_loop: asyncio.AbstractEventLoop, unused_tcp_port):
+        the_exception = Exception()
+
         class BadServer(DevServer):
             def handle(self, writer, request):
-                raise Exception()
+                raise the_exception
 
-        server = BadServer(self.directory)
+        def pass_if_matching(loop, context):
+            e = context['exception']
+            if e is not the_exception:
+                raise e
+
+        event_loop.set_exception_handler(pass_if_matching)
+
+        self.server = BadServer(self.directory)
         port = unused_tcp_port
-        server.serve('127.0.0.1', port, loop=event_loop)
+        self.server.serve('127.0.0.1', port, loop=event_loop)
         assert '500' in await get(f'http://127.0.0.1:{port}/../..')
 
 
